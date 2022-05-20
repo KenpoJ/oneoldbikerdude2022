4838bf5dd4c86202e302c0e2c53d90d13498e6e1
Jason Lyman <jason@lymannuthouose.com>
2022-05-19 21:11:36 -0700

    Added homepage layout. Styled Featured posts on homepage

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
     <link rel="canonical" href="https://<?php echo $_SERVER["HTTP_HOST"]; ?><?php echo parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ); ?>" />
     <link rel="preconnect" href="https://fonts.googleapis.com"> 
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-    <link href="https://fonts.googleapis.com/css2?family=Maiden+Orange&family=Poppins:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Poppins:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet">
     
     <script src="https://kit.fontawesome.com/53f8f7e502.js" crossorigin="anonymous"></script>
 
